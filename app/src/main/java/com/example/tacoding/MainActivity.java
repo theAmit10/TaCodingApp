e853272f59209f7f644bd7ccf1a1f3dc6953fb64
theamit10 <prasadak66@gmail.com>
2022-08-25 11:35:56 +0530

    created a bottom bar navigation

---
+++ app/src/main/java/com/example/tacoding/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.tacoding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.tacoding.Fragment.ContestFragment;
+import com.example.tacoding.Fragment.NewsFragment;
+import com.example.tacoding.Fragment.ProblemFragment;
+import com.example.tacoding.Fragment.UserFragment;
+import com.example.tacoding.databinding.ActivityMainBinding;
+import com.iammert.library.readablebottombar.ReadableBottomBar;
+
+public class MainActivity extends AppCompatActivity {
+
+     ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+
+        // used change from one transaction to anther transaction
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainer, new ContestFragment());
+        transaction.commit();
+
+        binding.bottomNavigation.setOnItemSelectListener(new ReadableBottomBar.ItemSelectListener() {
+            @Override
+            public void onItemSelected(int i) {
+
+                // this is used to change one fragment to another.
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                switch (i){
+                    case 0:
+                        transaction.replace(R.id.fragmentContainer, new ContestFragment());
+                        break;
+                    case 1:
+                        transaction.replace(R.id.fragmentContainer, new UserFragment());
+                        break;
+                    case 2:
+                        transaction.replace(R.id.fragmentContainer, new NewsFragment());
+                        break;
+                    case 3:
+                        transaction.replace(R.id.fragmentContainer, new ProblemFragment());
+                        break;
+                    case 4:
+                        transaction.replace(R.id.fragmentContainer, new ContestFragment());
+                        break;
+                }
+
+                transaction.commit();
+            }
+        });
+    }
+}
